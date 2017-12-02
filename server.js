a6b49575a44626dab4f96d94d2c71f28982f640a
Ed Urban <eddieurban2015@gmail.com>
2017-12-02 16:18:22 -0500

    REST API start
    -mysql package
    -rest apis

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "passw0rd"
+});
 
 var app = express();
 
@@ -12,9 +19,39 @@ app.use(express.static('src'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/', function (req, res) {
-  res.render('index')
+app.use(function(req, res, next) {
+    // log
+    console.log('Something is happening.');
+    next();
+});
+
+app.route('/anon_story')
+
+    // create a story (accessed at POST http://localhost:4000/anon_story)
+    .post(function(req, res) {
+
+        var story = new Story(); // create a new instance of the Bear model 
+		story.blob = req.body.blob; // 500 words max
+		story.rating = req.body.rating;
+
+        // save the bear and check for errors
+        story.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: ' created!' });
+        });
+
+    });
+
+app.get('/companies', function (req, res) {
+	
 })
 
 app.listen(4000);
 console.log('Umbrella server listening on port 4000.');
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to MySQL database!");
+});
